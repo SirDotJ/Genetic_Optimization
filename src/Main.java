11cd4b6b70eccd11fc8cd679da036772f7255070
SirDotJ <2020102045@pnu.edu.ru>
2024-01-10 12:28:59 +1000

    Перевёл код в Main из Test_EvolutionAlgorithm

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
+import genetic.EvolutionAlgorithm;
 
 public class Main{
     public static void main(String[] args) {
-
+        EvolutionAlgorithm algorithm;
+        if (args.length == 0)
+            algorithm = new EvolutionAlgorithm(100);
+        else {
+            try {
+                algorithm = new EvolutionAlgorithm(Integer.parseInt(args[0]));
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input, please enter number of populations to generate and try again...");
+                return;
+            }
+        }
+        algorithm.optimize();
+        System.out.println("Best genome: " + algorithm.getBestGenome());
+		System.out.println("Best genome population: " + algorithm.getBestGenomePopulation());
+		System.out.println("Best adaptibility: " + algorithm.getBestAdaptibility());
+		System.out.println("Best overall adaptibility population: " + algorithm.getBestOverallPopulation());
+		System.out.println("Best overall adaptibility: " + algorithm.getBestOverallAdaptibility());
     }
 }
